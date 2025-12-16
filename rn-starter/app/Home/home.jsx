0a89573bbe9b426c096b9f33611b0aae56694785
circular <taehyeonee823@dankook.ac.kr>
2025-12-16 23:25:22 +0900

    병합후 재충돌 해결

--- rn-starter/app/Home/home.jsx
+++ rn-starter/app/Home/home.jsx
@@ -314,15 +314,9 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginLeft: 20,
     marginRight: 20,
-<<<<<<< HEAD
-    padding: 20,
-    marginBottom: 10,
-    backgroundColor: '#F5F5F5',
-=======
     paddingHorizontal: 20,
     paddingVertical: 10,
     backgroundColor: '#F0F0F0',
->>>>>>> 3b4632ada183f11fd77ea387558f07aec50959b5
     borderRadius: 10,
   },
   missionText: {
